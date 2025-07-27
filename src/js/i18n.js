42eac2a4efa5a768d47e6a5d4851e184c3dbb601
oPyRuSo <opyruso@gmail.com>
2025-07-27 20:28:15 +0200

    fix translation formatting when value is not a string

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -11,6 +11,7 @@ function t(key, vars) {
   if(str === undefined) str = enTranslations[key];
   if(str === undefined) str = enGameTranslations[key];
   if(str === undefined) str = key;
+  if(typeof str !== 'string') str = str == null ? '' : String(str);
   if (vars) {
     for (const k in vars) {
       str = str.replace(`{${k}}`, vars[k]);
@@ -25,6 +26,7 @@ function tg(key, label, vars){
   if(str === undefined) str = enGameTranslations[key];
   if(str === undefined) str = enTranslations[key];
   if(str === undefined) str = label !== undefined ? label : key;
+  if(typeof str !== 'string') str = str == null ? '' : String(str);
   if (vars) {
     for (const k in vars) {
       str = str.replace(`{${k}}`, vars[k]);
@@ -34,6 +36,7 @@ function tg(key, label, vars){
 }
 
 function formatGameString(str){
+  if(typeof str !== 'string') str = str == null ? '' : String(str);
   return str
     .replace(/<br\s*\/?\s*>/gi, '<br/>')
     .replace(/<span class='([^']+)'>([^<]*)<\/span>/gi, (m, cls, txt) => {
